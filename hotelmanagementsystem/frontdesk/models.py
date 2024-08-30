60d28a7db31bc367e09d13e1171bdd7a1d44c126
abhiyanShrestha-fullstackdeveloper <abhiyanshrestha328@gmail.com>
2024-08-30 10:10:01 +0545

    small bug fixed and made sure that migration is done properly and added superuser also for admin panel

--- hotelmanagementsystem/frontdesk/models.py
+++ hotelmanagementsystem/frontdesk/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    name = models.CharField(255)
+    name = models.CharField(max_length=255)
     number = models.IntegerField()
     address = models.CharField(max_length=255)
     occupation = models.CharField(max_length=255)
